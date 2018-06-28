349bf085d8d4470b17e0d26d48e5d4a97654ce01
Unknown <joey.griffin@checkout.com>
2018-06-28 17:39:51 +0100

    Initial open api generator

---
+++ src/OpenApiGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi.Writers;
+
+namespace OpenApiGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(@"..\..\spec\swagger.1.yaml"))
+                {
+                    var document = new OpenApiStringReader().Read(sr.ReadToEnd(), out var diag);
+
+                    using (TextWriter writer = File.CreateText("spec.txt"))
+                    {
+                        document.SerializeAsV3(new OpenApiYamlWriter(writer));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
